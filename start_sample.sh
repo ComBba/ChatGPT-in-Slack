6c5e6e775be16155e170f8e1e6cd4a178171b532
ComBba <cent@naver.com>
2024-06-18 12:21:49 +0000

    start_sample.sh

---
+++ start_sample.sh
@@ -0,0 +1,44 @@
+# Create an app-level token with connections:write scope
+export SLACK_APP_TOKEN=xapp-1-...
+# Install the app into your workspace to grab this token
+export SLACK_BOT_TOKEN=xoxb-...
+# Visit https://platform.openai.com/account/api-keys for this token
+export OPENAI_API_KEY=sk-...
+
+# Optional: gpt-3.5-turbo and newer ones are currently supported (default: gpt-3.5-turbo)
+export OPENAI_MODEL=gpt-4o
+# Optional: Model temperature between 0 and 2 (default: 1.0)
+export OPENAI_TEMPERATURE=1
+# Optional: You can adjust the timeout seconds for OpenAI calls (default: 30)
+export OPENAI_TIMEOUT_SECONDS=60
+# Optional: You can include priming instructions for ChatGPT to fine tune the bot purpose
+export OPENAI_SYSTEM_TEXT="You proofread text. When you receive a message, you will check
+for mistakes and make suggestion to improve the language of the given text"
+# Optional: When the string is "true", this app translates ChatGPT prompts into a user's preferred language (default: true)
+export USE_SLACK_LANGUAGE=true
+# Optional: Adjust the app's logging level (default: DEBUG)
+export SLACK_APP_LOG_LEVEL=INFO
+# Optional: When the string is "true", translate between OpenAI markdown and Slack mrkdwn format (default: false)
+export TRANSLATE_MARKDOWN=true
+# Optional: When the string is "true", perform some basic redaction on prompts sent to OpenAI (default: false)
+export REDACTION_ENABLED=true
+# Optional: When the string is "true", this app shares image files with OpenAI (default: false)
+export IMAGE_FILE_ACCESS_ENABLED=true
+
+# To use Azure OpenAI, set the following optional environment variables according to your environment
+# default: None
+export OPENAI_API_TYPE=azure
+# default: https://api.openai.com/v1
+export OPENAI_API_BASE=https://YOUR_RESOURCE_NAME.openai.azure.com
+# default: None
+export OPENAI_API_VERSION=2023-05-15
+# default: None
+export OPENAI_DEPLOYMENT_ID=YOUR-DEPLOYMENT-ID
+
+# Experimental: You can try out the Function Calling feature (default: None)
+export OPENAI_FUNCTION_CALL_MODULE_NAME=tests.function_call_example
+
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+python main.py
